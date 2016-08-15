68490b1ff33f4fdbe56fdc3699573106ba868742
faiface <faiface@ksp.sk>
2016-08-15 22:10:44 +0200

    add picture loading

--- picture.go
+++ picture.go
@@ -1,5 +1,10 @@
 package gogame
 
+import (
+	"github.com/veandco/go-sdl2/sdl"
+	"github.com/veandco/go-sdl2/sdl_image"
+)
+
 // Picture provies a basic interface for all kinds of software/hardware surfaces/textures.
 type Picture interface {
 	// Size returns the width and height of a picture.
@@ -7,4 +12,41 @@ type Picture interface {
 
 	// ColorAt returns the color of pixel (x, y) of a picture.
 	ColorAt(x, y int) Color
+}
+
+// LoadPicture loads a picture stored in your filesystem at the specified path.
+// If an error occured during the loading (e.g. file does not exist), an error will be returned.
+func LoadPicture(path string) (Picture, error) {
+	var (
+		pic picture
+		err error
+	)
+	pic.surface, err = img.Load(path)
+	if err != nil {
+		return nil, err
+	}
+	return pic, nil
+}
+
+type picture struct {
+	surface   *sdl.Surface
+	texture   *sdl.Texture
+	rendIndex int
+}
+
+func (p picture) Size() (w, h int) {
+	return int(p.surface.W), int(p.surface.H)
+}
+
+func (p picture) ColorAt(x, y int) Color {
+	bpp := p.surface.BytesPerPixel()
+	index := y*int(p.surface.W) + x
+	pixel := p.surface.Pixels()[index*bpp : index*bpp+bpp]
+
+	rgba := [4]float64{0.0, 0.0, 0.0, 1.0}
+	for i := range pixel {
+		rgba[i] = float64(pixel[i]) / 255
+	}
+
+	return Color{rgba[0], rgba[1], rgba[2], rgba[3]}
 }
